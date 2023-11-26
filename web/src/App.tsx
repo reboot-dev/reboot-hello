9cf2302b51f5b6897536f8393b72fadbd5fb1214
Edgar Arturo Arrizon <98068287+edgar-arturo-arrizon@users.noreply.github.com>
2023-11-26 01:13:32 +0000

    added input field and handlers

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,34 +1,61 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import css from "./App.module.css";
 import { Chat } from "./gen/chat/v1/chat_rsm_react";
 import React from "react";
 
+interface Message {
+  id: number;
+  from_user: string;
+  text: string;
+}
+
+interface ChatRoomState {
+  chats: Message[];
+}
+
+interface PostRequest {
+  message: string;
+}
+
 function App() {
-  const { useGetAll } = Chat( {id : "test"} ); // storeMutationsLocallyInNamespace = ?
+  const [message, setMessage] = useState("");
+  const { useGetAll } = Chat({ id: "test" });
 
   const {
     response,
     isLoading,
-    mutations: { Post }, // any write methods exposed via mutations
+    mutations: { Post }
   } = useGetAll();
 
+
+  const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setMessage(event.target.value);
+  };
+
+  const handleSendMessage = () => {
+    if (message) {
+      Post({ message });
+      setMessage("");
+    }
+  };
+
   useEffect(() => {
-    console.log('initial render..')
+    console.log("initial render..");
+  }, []);
 
-  },[])
-
-  console.log('response: ', response)
+  console.log("response:", response);
 
   return (
     <div>
       {isLoading ? "Loading..." : ""}
-      <div className={css.messagesPage}>
-        chat box div
-        {
-        response ?
-        <>loading</> :
-        <> response is {response}</>
-        }
+      <div className={css.messageInput}>
+        <input
+          type="text"
+          value={message}
+          onChange={handleChange}
+          placeholder="Enter message"
+        />
+        <button onClick={handleSendMessage}>Send</button>
       </div>
     </div>
   );
