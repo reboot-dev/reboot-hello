91202eba1b5d406d23f19dc7608af8071e993041
Rebot <96078724+reboot-dev-bot@users.noreply.github.com>
2026-06-04 20:18:55 +0000

    [Release] Synchronize for release

--- .tests/serve_test.sh
+++ .tests/serve_test.sh
@@ -103,6 +103,7 @@ if command -v docker &> /dev/null; then
       --env=PORT=8787 \
       --env=RBT_STATE_DIRECTORY=/app/state/ \
       --env=RBT_SERVERS=2 \
+      --env=REBOOT_CRYPTO_ROOT_KEYS=v1:reboot-chat-room-serve-test \
       -p8787:8787 \
       --detach \
       $image_name \
